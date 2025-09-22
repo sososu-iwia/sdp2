25da29478324747f30ee09de052d7db582d97ee3
sososu-iwia <155320622+sososu-iwia@users.noreply.github.com>
2025-09-22 19:39:07 +0500

    Update OfficeLaptop.java

--- OfficeLaptop.java
+++ OfficeLaptop.java
@@ -1,6 +1,1 @@
-public final class OfficeLaptop implements Laptop {
-    @Override public void cpu()  { System.out.println("Office Laptop: efficient CPU"); }
-    @Override public void gpu()  { System.out.println("Office Laptop: integrated GPU"); }
-    @Override public void os()   { System.out.println("Office Laptop: OS installed"); }
-    @Override public void sold() { System.out.println("Office Laptop is on sale"); }
-}
+
